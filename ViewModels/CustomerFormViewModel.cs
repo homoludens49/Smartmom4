f63daf6900651bdd88c88104625623ce44ec24ec
Pjotrs Sakov <sakov.p@gmail.com>
2019-11-11 22:56:51 +0200

    A

---
+++ ViewModels/CustomerFormViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Smartmom4.Models;
+
+namespace Smartmom4.ViewModels
+{
+    public class CustomerFormViewModel
+    {
+        public IEnumerable<MembershipType> MembershipTypes { get; set; }
+        public Customer Customer { get; set; }
+    }
+}
